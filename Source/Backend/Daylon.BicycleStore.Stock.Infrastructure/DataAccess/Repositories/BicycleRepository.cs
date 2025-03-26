f544ce0b8d070c873dd58407ba23e0611875e37d
daylon <daylonfellipe3001@outlook.com>
2025-03-26 20:17:45 +0000

    WIP - Add Basic Functions on Repository

--- Source/Backend/Daylon.BicycleStore.Stock.Infrastructure/DataAccess/Repositories/BicycleRepository.cs
+++ Source/Backend/Daylon.BicycleStore.Stock.Infrastructure/DataAccess/Repositories/BicycleRepository.cs
@@ -10,6 +10,38 @@ namespace Daylon.BicycleStore.Stock.Infrastructure.DataAccess.Repositories
 
         public BicycleRepository(BicycleStoreDbContext dbContext) => _dbContext = dbContext;
 
+        // DB
+
+        public async Task SaveChangesAsync() => await _dbContext.SaveChangesAsync();
+
+        public async Task AddAsync(Bicycle bicycle) => await _dbContext.Bicycles.AddAsync(bicycle);
+
+        public async Task UpdateTaskAsync(Bicycle bicycle)
+        {
+            _dbContext.Bicycles.Update(bicycle);
+            await SaveChangesAsync();
+        }
+
+        // GET
         public async Task<List<Bicycle>> GetBicyclesAsync() => await _dbContext.Bicycles.ToListAsync();
+
+        public async Task<Bicycle> GetBicycleByIdAsync(Guid id)
+        {
+            var bicyble = await _dbContext.Bicycles.FirstOrDefaultAsync(b => b.Id == id);
+
+            return bicyble ?? throw new Exception($"Bicycle with id {id} not found");
+        }
+
+        // DELETE
+
+        public async Task DeleteBicycleByIdAsync(Guid id)
+        {
+            var bicycle = await GetBicycleByIdAsync(id);
+
+            _dbContext.Bicycles.Remove(bicycle);
+
+            await SaveChangesAsync();
+        }
     }
 }
+ 
