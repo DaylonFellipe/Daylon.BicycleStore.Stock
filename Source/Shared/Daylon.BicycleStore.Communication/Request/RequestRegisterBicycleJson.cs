84bc0b8b799acc1093f9f6045941715af0c27ac5
daylon <daylonfellipe3001@outlook.com>
2025-03-27 19:52:08 +0000

    ADD - Add Project Communication and RequestRegister

---
+++ Source/Shared/Daylon.BicycleStore.Communication/Request/RequestRegisterBicycleJson.cs
@@ -0,0 +1,20 @@
+﻿using Daylon.BicycleStore.Stock.Domain.Entity.Enum;
+
+namespace Daylon.BicycleStore.Communication.Request
+{
+    public class RequestRegisterBicycleJson
+    {
+        // Basic Properties
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; } = string.Empty;
+
+        // Technical Properties
+        public BrandEnum Brand { get; set; }
+        public ModelEnum Model { get; set; }
+        public ColorEnum Color { get; set; }
+
+        // Stock Properties
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
